611733120ac38cdf5602338101edf87a5620790e
Behrouz <bkhezry@gmail.com>
2019-07-10 17:22:28 +0430

    add dark mode to app - update libraries to last version appcomapt 1.1.0-rc01 & material components to 1.1.0-alpha07 - change style to material component daynight.

--- app/src/main/java/com/yariksoffice/languagetest/ui/MainActivity.java
+++ app/src/main/java/com/yariksoffice/languagetest/ui/MainActivity.java
@@ -2,8 +2,12 @@ package com.yariksoffice.languagetest.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
 import com.yariksoffice.languagetest.R;
 import com.yariksoffice.languagetest.TestService;
@@ -22,7 +26,19 @@ public class MainActivity extends BaseActivity {
         findViewById(R.id.activity_2).setOnClickListener(v -> startActivity(new Intent(this, TestActivity2.class)));
         findViewById(R.id.service).setOnClickListener(v -> startService(new Intent(this, TestService.class)));
         findViewById(R.id.settings).setOnClickListener(v -> startActivity(new Intent(this, SettingsActivity.class)));
+        SwitchCompat switchCompat = findViewById(R.id.dark_mode_switch);
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    getDelegate().setLocalNightMode(
+                        AppCompatDelegate.MODE_NIGHT_YES);
+                else
+                    getDelegate().setLocalNightMode(
+                        AppCompatDelegate.MODE_NIGHT_NO);
 
+            }
+        });
         TextView tv = findViewById(R.id.hello);
         String date = SimpleDateFormat.getDateInstance().format(new Date());
         tv.setText(getString(R.string.hello, date));
